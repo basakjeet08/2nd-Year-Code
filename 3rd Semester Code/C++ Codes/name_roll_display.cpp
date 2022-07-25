49b5e8fe63eb0509c14ba91acb7a402c0df9586a
Anirban Basak <jeetbasak2002@gmail.com>
2022-07-25 22:27:20 +0530

    25/07/2022

--- 3rd Semester Code/C++ Codes/name_roll_display.cpp
+++ 3rd Semester Code/C++ Codes/name_roll_display.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
+struct Student{
+    string name;
+    int roll;
+};
 int main(){
-    string name[10];
-    int roll_no[10];
+    struct Student st[2];
     int i ;
-    for(i = 0;i<10;i++){
+    for(i = 0;i<2;i++){
     cout<<"Enter Your Name : ";
-    getline(cin,name[i]);
+    getline(cin,st[i].name);
     cout<<"Enter Your Roll No : ";
-    cin>>roll_no[i];
+    cin>>st[i].roll;
+    cin.ignore();
     }
-
-    for(i = 0 ;i<10;i++){
-        cout<<"\n\nYour Name is : "<<name[i]<<"\n";
-        cout<<"Your Roll No is : "<<roll_no[i]<<"\n";
+    for(i = 0 ;i<2;i++){
+        cout<<"\n\nYour Name is : "<<st[i].name<<"\n";
+        cout<<"Your Roll No is : "<<st[i].roll<<"\n";
     }
-    
-
     return 0;
 }
