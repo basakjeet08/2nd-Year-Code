a60bc0a779412f7f1a624753e46cf02ee1cd11af
Anirban Basak <jeetbasak2002@gmail.com>
2022-07-29 21:59:38 +0530

    29/07/2022

--- 3rd Semester Code/C language Code/doubts.c
+++ 3rd Semester Code/C language Code/doubts.c
@@ -1,69 +1,73 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-struct student
-{
-    char name[20];
-    int roll;
-    int marks[5];
-    int tot;
-    char grade;
+#include <stdio.h>
+struct ArrivalTime{
+        int hours;
+        int minutes;
+        int seconds;
+        char AM_PM[3];
+    };
+struct TrainDetails{
+    char name[100];
+    struct ArrivalTime time;
 };
-typedef struct student node;
-void main()
-{
-    node *x;
-    int i,j,n,t;
-    printf("\nHow many records you want to input");
-    scanf("%d",&n);
-    x=(node *)malloc(n*sizeof(node));
-    for(i=0;i<n;i++)
-    {
-        fflush(stdin);
-        printf("\nEnter name of the student: ");
-        gets(x[i].name);
-        printf("\nEnter roll number of the student: ");
-        scanf("%d",&x[i].roll);
-        printf("\nEnter marks of 5 subjects:\n");
-        for(j=0;j<5;j++)
-            scanf("%d",&x[i].marks[j]);
+int main(){
+    struct TrainDetails trains[5];
+    int i;
+    printf("Enter train details:\n");
+    for (i=0; i<2; i++){
+        printf("For train %d:\n", i+1);
+        printf("Enter train name: ");
+        getchar();
+        gets(trains[i].name);
+        printf("Enter arrival time of train:\n");
+        printf("Enter hours: ");
+        scanf("%d", &trains[i].time.hours);
+        printf("Enter minutes: ");
+        scanf("%d", &trains[i].time.minutes);
+        printf("Enter seconds: ");
+        scanf("%d", &trains[i].time.seconds);
+        printf("AM / PM: ");
+        getchar();
+        gets(trains[i].time.AM_PM);
+        printf("\n");
     }
-    for(i=0;i<n;i++)
-    {
-        x[i].tot=0;
-        for(j=0;j<5;j++)
-            x[i].tot+=x[i].marks[j];
-        if(x[i].tot>=400)
-            strcpy(x[i].grade,"A");
-        if(x[i].tot>=300)
-            strcpy(x[i].grade,"B");
-        if(x[i].tot>=200)
-            strcpy(x[i].grade,"C");
-        else
-            strcpy(x[i].grade,"D");
+
+
+
+
+
+
+
+    for(int i = 0;i<5;i++){
+        int b = trains[i].time.AM_PM == "PM";
+        printf("%d\n",b);
     }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n-i;j++)
-        {
-            if(x[j].tot>x[j+1].tot)
-            {
-                t=x[j].tot;
-                x[j].tot=x[j+1].tot;
-                x[j+1].tot=t;
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    printf("Train details:\n");
+    for (i=0; i<5; i++){
+        printf("Train %d: ", i+1);
+        printf("Name: %s\n", trains[i].name);
+
+        if (trains[i].time.AM_PM[0] == 'P')
+            if (trains[i].time.hours != 12)
+                trains[i].time.hours += 12;
+        
+        else{
+            if (trains[i].time.hours == 12)
+                trains[i].time.hours = 0;
         }
+        printf("Arrival Time: %d:%d:%d\n", trains[i].time.hours, trains[i].time.minutes, trains[i].time.seconds);
     }
-    for(i=0;i<n;i++)
-    {
-        fflush(stdin);
-        printf("\n\nName = %s",x[i].name);
-        printf("\nRoll Number = %d",x[i].roll);
-        printf("\nMarks of 5 subjects:\n");
-        for(j=0;j<5;j++)
-            printf("%d\n",x[i].marks[j]);
-        printf("\nTotal Marks = %d",x[i].tot);
-        printf("\nGrade = %s",x[i].grade);
-    }
+    return 0;
 }
