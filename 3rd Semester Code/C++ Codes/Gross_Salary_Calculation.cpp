65d6ded8197ea8c1f5dcf6631bcd4d94023123b8
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-02 09:44:41 +0530

    02/08/2022

--- 3rd Semester Code/C++ Codes/Gross_Salary_Calculation.cpp
+++ 3rd Semester Code/C++ Codes/Gross_Salary_Calculation.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
-#include <string>
-using namespace std;
-struct Employee{
-    int id;
+#include <iomanip>
+using namespace std ;
+struct Employee {
+    int Id;
     string name;
     int age;
     float basic_salary;
     float gross_salary;
 };
-void getData(int , struct Employee*);
-void display(int , struct Employee*);
+void calShow(struct Employee*,int);
+void getData(struct Employee*,int);
 int main(){
     int no_of_employees;
-
-    cout<< "Enter the Number of Employees : ";
+    cout << "Enter the number of the Employees : ";
     cin >> no_of_employees;
+    cin.ignore();
     struct Employee emp[no_of_employees];
-
-    getData(no_of_employees,emp);
-    display(no_of_employees,emp);
+    getData(emp,no_of_employees);
+    calShow(emp,no_of_employees);
     return 0;
 }
-
-void getData(int no_of_employees,struct Employee *emp){
-
-    for (int i = 0;i<no_of_employees;i++)
-    {
-        cout << "Enter the Employee ID : ";
-        cin >> emp->id;
+void getData(struct Employee* emp,int no_of_employees){
+    for(int i=0;i<no_of_employees;i++){
+        cout << "Enter the Details of Employee "<<i+1 << " : \n";
+        cout << "ID : ";
+        cin >> emp->Id;
         cin.ignore();
-        cout << "Enter the Employee Name : ";
+        cout << "Name : ";
         getline(cin,emp->name);
-        cout << "Enter the Employee age : ";
+        cout << "Age : ";
         cin >> emp->age;
-        cout << "Enter the Employee Basic Salary : ";
+        cout << "Basic Salary : ";
         cin >> emp->basic_salary;
-        // Doing the Calculation Part and initializing the Gross Salary 
-        emp->gross_salary = emp->basic_salary + (80.0/100.0 )*(emp->basic_salary ) + (10/100.0)*(emp->basic_salary);
+        cout << "\n";
         emp++;
     }
 }
-
-void display(int no_of_employees,struct Employee* emp){
-    cout << "\n\n\n";
-        for (int i = 0;i<no_of_employees;i++)
-    {
-        cout << "The Employee ID : " << emp->id << "\n";
-        cout << "The Employee Name : " << emp->name << "\n";
-        cout << "The Employee age : " << emp->age << "\n";
-        cout << "The Employee Basic Salary : " << emp->basic_salary << "\n";
-        cout << "The Employee Gross Salary : " << emp->gross_salary << "\n\n\n";
+void calShow(struct Employee* emp,int no_of_students){
+    cout << left << setw(12) << "ID";
+    cout << left << setw(20) << "NAME";
+    cout << left << setw(9) << "AGE";
+    cout << left << setw(18) << "Basic Salary";
+    cout << left << setw(18) << "Gross Salary";
+    cout << endl;
+    for(int i=0;i<no_of_students;i++){
+        emp->gross_salary = emp->basic_salary+(80.0/100)*emp->basic_salary+ (10.0/100.0)*emp->basic_salary;
+        cout << left << setw(12) << emp->Id;
+        cout << left << setw(20) << emp->name;
+        cout << left << setw(9) << emp->age;
+        cout << left << setw(18) << emp->basic_salary;
+        cout << left << setw(18) << emp->gross_salary;
+        cout << endl;
         emp++;
     }
 }
