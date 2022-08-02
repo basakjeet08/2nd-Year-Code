cfc7bff5fb704273f345a1588040e554267e6f13
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-02 14:52:20 +0530

    02/08/2022

--- 3rd Semester Code/C++ Codes/Student_Roll_Marks_Percent.cpp
+++ 3rd Semester Code/C++ Codes/Student_Roll_Marks_Percent.cpp
@@ -1,187 +1,134 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-struct Student
-{
+struct Student {
     string name;
-    int roll_no;
-    int marks_in_phy, marks_in_eng, marks_in_maths, marks_in_comp, marks_in_chem;
+    int roll;
+    int marks[5];
     int total_marks;
     float percentage;
 };
-
-// Prototypes of the Functions
-void calculateTotalAndPercentage(int, struct Student *);
-void getData(int, struct Student *);
-void showOutputOfParticularStudent(int, struct Student *);
-void showOutputOfParticularPercentageRange(int, struct Student *);
-void menuOfTheProgram(int , struct Student*);
-void showOutputofAllStudents(int , struct Student *);
-void sortTheArray(int,struct Student*);
-
-// Main Fucntion of the Code
-int main()
-{
-    int no_of_students;
-
-    cout << "Enter the Number of Students : ";
-    cin >> no_of_students;
-
-    struct Student stu[no_of_students];
-    getData(no_of_students, stu);
-    calculateTotalAndPercentage(no_of_students, stu);
-    menuOfTheProgram(no_of_students,stu);
-    return 0;
+void getData(struct Student*,int);
+void findTotalPercentage(struct Student*,int);
+void menuOfTheProgram(struct Student*,int);
+void showData(struct Student*,int);
+void showData(struct Student*,int,int);
+void showData(struct Student*,int,float,float);
+void sortArray(struct Student*,int );
+int main(){
+    int student_count;
+    cout << "Enter the number of Students : ";
+    cin >> student_count;
+    cin.ignore();
+    struct Student stu[student_count];
+    getData(stu,student_count);
+    findTotalPercentage(stu,student_count);
+    menuOfTheProgram(stu,student_count);
+    return 0 ;
 }
-
-// Function is made to Get Data as Input fromm User
-void getData(int no_of_students, struct Student *stu)
-{
-    for (int i = 0; i < no_of_students; i++)
-    {
-        cout << "Enter the Name : ";
-        cin.ignore();
+void getData(struct Student* stu,int student_count){
+    for(int i = 0;i<student_count;i++){
+        cout << "Enter the Details of Student " << i+1 << " : \n";
+        cout << "Name : ";
         getline(cin,stu->name);
-        cout << "Enter the Roll No : ";
-        cin >> stu->roll_no;
-        cout << "Enter the Marks in Physics : ";
-        cin >> stu->marks_in_phy;
-        cout << "Enter the Marks in Maths : ";
-        cin >> stu->marks_in_maths;
-        cout << "Enter the Marks in English : ";
-        cin >> stu->marks_in_eng;
-        cout << "Enter the Marks in Computer : ";
-        cin >> stu->marks_in_comp;
-        cout << "Enter the Marks in Chemistry : ";
-        cin >> stu->marks_in_chem;
-
+        cout << "Roll No : ";
+        cin >> stu->roll;
+        cout << "Enter Marks in 5 subjects with spaces inbetween them :--";
+        for(int j=0;j<5;j++)
+            cin >> stu->marks[j];
+        cout << endl;
+        cin.ignore();
         stu++;
     }
 }
-
-// Calculating the total marks and the percentage of every Students
-void calculateTotalAndPercentage(int no_of_students, struct Student *stu)
-{
-    int i = 0;
-    for (i = 0; i < no_of_students; i++)
-    {
-        stu->total_marks = stu->marks_in_chem + stu->marks_in_comp + stu->marks_in_eng + stu->marks_in_maths + stu->marks_in_phy;
-        stu->percentage = (float)stu->total_marks / 5.0;
-
-        stu++;
-    }
-}
-
-// Menu of the program to ask the User what to do In the program so this Program runs according to Him !!
-void menuOfTheProgram(int no_of_students , struct Student *stu)
-{
-    int choice_by_user;
+void menuOfTheProgram(struct Student* stu,int student_count){
+    int choice ;
     do{
+        cout << "\n";
+        cout << "-----------------------------------------------------------\n";
+        cout << "1. Show Details of all the Students\n";
+        cout << "2. Show details of a student with a given Roll NO. \n";
+        cout << "3. Display Details of Student who are in-between a given percent range\n";
+        cout << "4. Sort the Array in a ascending Order \n";
+        cout << "5. Exit\n";
+        cout << "\nEnter Your Choice : ";
+        cin >> choice ;
 
-        cout << "\n\n\n----------------------------Menu------------------------------\n";
-        cout << "1. Show the Total marks and the percentage of the Student along with other details :-\n";
-        cout << "2. Find Details of a particular Student By entering Roll No.:-\n";
-        cout << "3. Display the Names of Student who are in your entered Percentage Range:-\n";
-        cout << "4. Sort the Array in Assending Order and Display it :-\n";
-        cout << "5. Exit the Program:-\n";
-        cout << "Enter Your Choice : ";
-        cin >>choice_by_user;
-
-        switch (choice_by_user)
-        {
-        case 1:
-        showOutputofAllStudents(no_of_students,stu);
-        break;
-        case 2:
-        showOutputOfParticularStudent(no_of_students , stu);
-        break;
-        case 3:
-        showOutputOfParticularPercentageRange(no_of_students,stu);
-        break;
-        case 4:
-        sortTheArray(no_of_students,stu);
-        showOutputofAllStudents(no_of_students,stu);
-        break;
-        case 5 :
-        break;
-        default:
-        cout << "Not a Valid Choice :-\n";
+        switch(choice){
+            case 1 :
+                showData(stu,student_count);
+                break;
+            case 2 :
+                int roll;
+                cout << "Enter the Roll No : ";
+                cin >> roll;
+                showData(stu,student_count,roll);
+                break;
+            case 3 :
+                float ub,lb;
+                cout << "Enter the Lower Bound of Percentage : ";
+                cin >> lb;
+                cout << "Enter the Upper Bound of Percentage : ";
+                cin >> ub;
+                showData(stu,student_count,lb,ub);
+                break;
+            case 4 :
+                sortArray(stu,student_count);
+                showData(stu,student_count);
+                break;
+            case 5 : 
+                break;
+            default :
+                cout << "Wrong Choice !! Try Again\n";
         }
-    }while(choice_by_user!=5);
+    }while(choice!=5);
 }
-
-void showOutputofAllStudents(int no_of_students , struct Student *stu){
-    cout << "\n\nDetails of All Students :----------\n";
-    for(int i=0;i<no_of_students;i++){
-        cout << "The Name : "<< stu->name << "\n";
-        cout << "The Roll No : "<< stu->roll_no << "\n";
-        cout << "The Marks in Physics : "<< stu->marks_in_phy << "\n";
-        cout << "The Marks in Maths : "<< stu->marks_in_maths << "\n";
-        cout << "The Marks in English : "<< stu->marks_in_eng << "\n";
-        cout << "The Marks in Computer : "<< stu->marks_in_comp << "\n";
-        cout << "The Marks in Chemistry : "<< stu->marks_in_chem << "\n";
-        cout << "The total marks : " << stu->total_marks << "\n";
-        cout << "The total Percentage : " << stu->percentage << "\n\n\n";
-
+void findTotalPercentage(struct Student* stu,int student_count){
+    for(int i = 0;i<student_count;i++){
+        stu->total_marks = stu->marks[0]+stu->marks[1]+stu->marks[2]+stu->marks[3]+stu->marks[4];
+        stu->percentage = (float)stu->total_marks/5.0;
         stu++;
     }
 }
-
-void showOutputOfParticularStudent(int no_of_students, struct Student *stu){
-    int roll_to_search ;
-    int i,count = 0;
-
-    cout << "\n\n Input the Roll u want to see Details of :-";
-    cin >> roll_to_search;
-    for(i=0;i<no_of_students;i++){
-        if(stu->roll_no == roll_to_search){
-            count = 1;
-            break;
+void showData(struct Student *stu,int student_count,int given_roll){
+    int flag = 0;
+    for(int i = 0;i<student_count;i++){
+        if(stu->roll == given_roll){
+            showData(stu,1);
+            flag = 1;
         }
         stu++;
     }
-    if(count){
-        cout << "Found Out Something Check it out :--\n";
-        cout << "The Name : " << stu->name << "\n";
-        cout << "The Roll No : " << stu->roll_no << "\n";
-        cout << "The Marks in Physics : " << stu->marks_in_phy << "\n";
-        cout << "The Marks in Maths : " << stu->marks_in_maths << "\n";
-        cout << "The Marks in English : " << stu->marks_in_eng << "\n";
-        cout << "The Marks in Computer : " << stu->marks_in_comp << "\n";
-        cout << "The Marks in Chemistry : " << stu->marks_in_chem << "\n";
-        cout << "The total marks : " << stu->total_marks << "\n";
-        cout << "The total Percentage : " << stu->percentage << "\n\n\n";
-    }
-    else
-        printf("No Such Directories Found !!");
+    if(!flag)
+        cout << "No Data Found !! \n";
 }
-
-void showOutputOfParticularPercentageRange(int no_of_students,struct Student *stu){
-    float lower_range,upper_range;
-    cout << "\n\n Enter the Lower Range of Percentage : ";
-    cin >> lower_range;
-    cout << "\n\n Enter the Upper Range of Percentage : ";
-    cin >> upper_range;
-
-    for(int i = 0;i<no_of_students;i++){
-        if(lower_range<=(stu->percentage) && upper_range>= (stu->percentage)){
-        cout << "Enter the Name : " << stu->name << "\n";
-        cout << "Enter the Roll No : " << stu->roll_no << "\n";
-        cout << "Enter the Marks in Physics : "<< stu->marks_in_phy << "\n";
-        cout << "Enter the Marks in Maths : " << stu->marks_in_maths << "\n";
-        cout << "Enter the Marks in English : " << stu->marks_in_eng << "\n";
-        cout << "Enter the Marks in Computer : " << stu->marks_in_comp << "\n";
-        cout << "Enter the Marks in Chemistry : " << stu->marks_in_chem << "\n\n\n";
-        }
+void showData(struct Student *stu,int student_count,float lb,float ub){
+    for(int i=0;i<student_count;i++){
+        if((stu->percentage >= lb) && (stu->percentage<=ub))
+            showData(stu,1);
         stu++;
     }
 }
-void sortTheArray(int no_of_students,struct Student *stu){
+void showData(struct Student* stu, int student_count){
+    for(int i = 0;i<student_count;i++){
+        cout << "Details of Student "<< i+1 <<" : \n";
+        cout << "Name : " << stu->name << endl;
+        cout << "Roll : " << stu->roll << endl ;
+        cout << "Marks : ";
+        for(int j = 0;j<5;j++)
+            cout << stu->marks[j]<<" ";
+        cout << "\n";
+        cout << "Total Marks : " << stu->total_marks << endl;
+        cout << "Percentage : " << stu->percentage << endl ;
+        cout << "\n\n";
+        stu++;
+    }
+}
+void sortArray(struct Student *stu,int student_count){
     struct Student temp;
     struct Student *pointer = stu;
-    for(int i = 0;i<no_of_students-1;i++){
-        for(int j=0;j<no_of_students-1-i;j++){
+    for(int i = 0;i<student_count-1;i++){
+        for(int j=0;j<student_count-1-i;j++){
             if(stu->total_marks>=(stu+1)->total_marks){
                 temp = *stu;
                 *stu = *(stu+1);
