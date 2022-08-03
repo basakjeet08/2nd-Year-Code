8554232cb8fbbed3be075808327590df7fbcedf0
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-03 09:38:18 +0530

    03/08/2022

---
+++ 3rd Semester Code/C++ Codes/Student_roll_marks_class.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student{
+    private :
+        int roll,marks;
+        string name ;
+    public : 
+        void getData(int i){
+            cout << "\nEnter the Details of Student "<<i+1<<" : \n";
+            cout << "Name : ";
+            getline(cin,name);
+            cout << "Roll No. : ";
+            cin >> roll;
+            cout << "Total Marks : ";
+            cin >> marks;
+            cin.ignore();
+        }
+        void showData(int i){
+            cout << "\nDetails of Student " << i+1 << " : \n";
+            cout << "Name : " << name <<endl;
+            cout << "Roll No. : " << roll << endl;
+            cout << "Total Marks : " << marks << endl ;
+        }
+};
+int main(){
+    int nos;
+    cout << "Enter the Number of Students : ";
+    cin >> nos;
+    cin.ignore();
+    Student stu[nos] ;
+    for(int i=0;i<nos;i++)
+        stu[i].getData(i);
+    for(int i=0;i<nos;i++)
+        stu[i].showData(i);
+    return 0;
+}
