59d7c28111022045f72ff048b5bfcb4733dacbbb
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-09 11:19:06 +0530

    09/08/2022

--- 3rd Semester Code/C language Code/Square_Matrix.c
+++ 3rd Semester Code/C language Code/Square_Matrix.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 void nonZeroElements(int* ,int );
+void aboveLeadingDiagonal(int *, int );
+void showMinorElements(int * , int );
+void mulDiagonalElement(int * , int );
 int main(){
     int nos;
     printf("Enter the Size of Array : ");
@@ -12,6 +15,9 @@ int main(){
             scanf("%d",&arr[i][j]);
     }
     nonZeroElements(arr[0],nos);
+    aboveLeadingDiagonal(arr[0],nos);
+    showMinorElements(arr[0],nos);
+    mulDiagonalElement(arr[0],nos);
     return 0 ;
 }
 void nonZeroElements(int *arr , int nos){
@@ -21,5 +27,35 @@ void nonZeroElements(int *arr , int nos){
             count++;
         arr++;
     }
-    printf("\nThe Number of Non-Zero Elements are : %d",count);
+    printf("\nThe Number of Non-Zero Elements : %d\n",count);
+}
+void aboveLeadingDiagonal(int *arr,int nos){
+    int sum = 0;
+    for(int i = 0;i<nos;i++)
+        for(int j = 0;j<nos;j++){
+            if(j>i)
+                sum+= *(arr);
+            arr++;
+        }
+    printf("The Sum of Elements above Leading Diagonal : %d\n",sum);
+}
+void showMinorElements(int * arr , int nos){
+    printf("The Elements below Minor Diagonal : ");
+    for(int i = 0;i<nos;i++)
+        for(int j = 0;j<nos;j++){
+            if(i>j)
+                printf("%d ",*(arr));
+            arr++;
+        }
+    printf("\n");
+}
+void mulDiagonalElement(int * arr , int nos){
+    int mul = 1;
+    for(int i = 0;i<nos;i++)
+        for(int j = 0;j<nos;j++){
+            if(j == i)
+                mul*=*(arr);
+            arr++;
+        }
+    printf("The Product of Diagonal Elements are : %d",mul);
 }
