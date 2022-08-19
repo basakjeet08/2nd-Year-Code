3f209102530e9c17323cef74271dfd181bf44906
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-19 13:51:05 +0530

    19/08/2022

--- 3rd Semester Code/C language Code/doubts.c
+++ 3rd Semester Code/C language Code/doubts.c
@@ -1,123 +1,40 @@
-#include<stdio.h>
-#include<string.h>
-struct date
+#include <stdio.h>
+#include <stdlib.h>
+void input(int *,int);
+void main()
 {
-    int day;
-    int month;
-    int year;
-};
-struct employee
+    int n_800;
+    printf ("enter the no. of elements in an array :");
+    scanf ("%d", &n_800);
+    int *a_800;
+    a_800=(int*)malloc(n_800*sizeof(int));
+    input (a_800 ,n_800);
+}
+void input(int *a_800 , int n_800)
 {
-    char name[20];
-    struct date dob;
-    struct date doj;
-    int salary;
-};
-int main()
+    printf ("no. of elements in an array :");
+    for(int i_800=0;i_800<n_800;i_800++)
+        scanf ("%d",(a_800)+i_800);
+}
+void display(int *a_800 , int n_800)
 {
-    int i,j;
-    struct employee e[5],temp,temp1,temp2,temp3;
-    printf("Date of Birth\n");
-    for(i=0;i<5;i++)
-    {
-        printf("For employee %d enter the day: ",i+1);
-        scanf("%d",&e->dob.day);
-        printf("For employee %d enter the month: ",i+1);
-        scanf("%d",&e->dob.month);
-        printf("For employee %d enter the year: ",i+1);
-        scanf("%d",&e->dob.year);
-        printf("\n");
-
-        printf("Date Of Joining of employee\n");
-        printf("For employee %d enter the day: ",i+1);
-        scanf("%d",&e->doj.day);
-        printf("For employee %d enter the month: ",i+1);
-        scanf("%d",&e->doj.month);
-        printf("For employee %d enter the year: ",i+1);
-        scanf("%d",&e->doj.year);
-        printf("\n");
-        
-        printf("Employee %d name: ",i+1);
-        scanf("%s",e[i].name);
-        printf("\n");
-        printf("For employee %d salary: ",i+1);
-        scanf("%d",&e[i].salary);
-    }
-    //Sorting By name
-    for (i = 1; i < 5; i++)
-    {
-        for (j = 0; j < 5 - i; j++) 
-        {
-            if (strcmp(e[j].name, e[j + 1].name) > 0) 
-            {
-                temp = e[j];
-                e[j] = e[j + 1];
-                e[j + 1] = temp;
-            }
-        }
-    }
-    printf("\n");
-    //Printing the sorted by name structure
-    for(i=0;i<5;i++)
-    {
-        printf("Name: %s\nDOB: %d/%d/%d\nDOJ: %d:%d:%d\nSalary: %d\n",e[i].name,e[i].dob.day,e[i].dob.month,e[i].dob.year,e[i].doj.day,e[i].doj.month,e[i].doj.year,e[i].salary);
-    }
-    //Sort by DOB
-    for (i=0; i<4; i++)
-    {    
-        for (j=i+1; j<5; j++)
-        {       
-            if ((e[i].dob.year > e[j].dob.year)&&(e[i].dob.month > e[j].dob.month)&&(e[i].dob.day > e[j].dob.day))
-            {          
-                temp1=e[i];          
-                e[i] = e[j];          
-                e[j] = temp1;
-            }
-        }
-    }
-    printf("\n");
-    //Printing the sorted by DOB
-    for(i=0;i<5;i++)
-    {
-        printf("Name: %s\nDOB: %d/%d/%d\nDOJ: %d:%d:%d\nSalary: %d\n",e[i].name,e[i].dob.day,e[i].dob.month,e[i].dob.year,e[i].doj.day,e[i].doj.month,e[i].doj.year,e[i].salary);
-    }
-    //Sort by DOJ
-    for (i=0; i<4; i++)
-    {    
-        for (j=i+1; j<5; j++)
-        {       
-            if ((e[i].doj.year > e[j].doj.year)&&(e[i].doj.month > e[j].doj.month)&&(e[i].doj.day > e[j].doj.day))
-            {          
-                temp2=e[i];          
-                e[i] = e[j];          
-                e[j] = temp2;
-            }
-        }
-    }
-    printf("\n");
-    //Printing the sorted by DOJ 
-    for(i=0;i<5;i++)
-    {
-        printf("Name: %s\nDOB: %d/%d/%d\nDOJ: %d:%d:%d\nSalary: %d\n",e[i].name,e[i].dob.day,e[i].dob.month,e[i].dob.year,e[i].doj.day,e[i].doj.month,e[i].doj.year,e[i].salary);
-    }
-    //Sort by salary
-    for (i=0; i<4; i++)
-    {    
-        for (j=i+1; j<5; j++)
-        {       
-            if (e[i].salary > e[j].salary)
-            {          
-                temp3=e[i];          
-                e[i] = e[j];          
-                e[j] = temp3;
-            }
-        }
-    }
-    //Printing the sorted by salary
-    for(i=0;i<5;i++)
-    {
-        printf("Name: %s\nDOB: %d/%d/%d\nDOJ: %d:%d:%d\nSalary: %d\n",e[i].name,e[i].dob.day,e[i].dob.month,e[i].dob.year,e[i].doj.day,e[i].doj.month,e[i].doj.year,e[i].salary);
-        printf("\n");
-    }
-    return 0;
+    printf ("the array is :\n");
+    for(int  i_800=0;i_800<n_800;i_800++)
+    printf ("%3d", *(a_800+i_800));
+}
+void search(int *a_800 , int n_800)
+{
+    int s_800, u_800=0;
+printf("enter the no.that you want to search :");
+scanf ("%d",&s_800);
+for( int i_800=0;i_800<n_800;i_800++)
+{
+  if(s_800== *(a_800+i_800))
+  {
+    printf("the no. found at %d",(i_800+1));
+    u_800++; 
+  }
+}
+if (u_800==0)
+printf("\n no. not found");
 }
