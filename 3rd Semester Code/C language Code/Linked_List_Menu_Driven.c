f2e6c636a03e490d2c5948e50fc09c20cc448b55
Anirban Basak <jeetbasak2002@gmail.com>
2022-09-02 12:56:31 +0530

    02/09/2022

--- 3rd Semester Code/C language Code/Linked_List_Menu_Driven.c
+++ 3rd Semester Code/C language Code/Linked_List_Menu_Driven.c
@@ -8,6 +8,13 @@ void menuOfTheProgram(struct Node* , struct Node* , struct Node*);
 struct Node* createNode(struct Node*);
 void searchElement(struct Node*);
 void displayList(struct Node*);
+struct Node* insert_first(struct Node* );
+void insert_last(struct Node* );
+void insert_pos(struct Node*);
+struct Node* delete_first(struct Node*);
+void delete_last(struct Node*);
+void delete_pos(struct Node*);
+
 int main(){
     int count = 1;
     int flag = 0;
@@ -27,7 +34,9 @@ void menuOfTheProgram(struct Node* head , struct Node* ptr , struct Node* New){
         printf("1. Create another Node \n");
         printf("2. Search for an Element \n");
         printf("3. Display the Linked List \n");
-        printf("4. Exit \n");
+        printf("4. Insert an Element \n");
+        printf("5. Delete an Element \n");
+        printf("6. Exit \n");
         scanf("%d",&choice);
 
         switch(choice){
@@ -41,6 +50,38 @@ void menuOfTheProgram(struct Node* head , struct Node* ptr , struct Node* New){
                 displayList(head);
                 break;
             case 4 :
+                printf("1. Insert at first \n");
+                printf("2. Insert at Last \n");
+                printf("3. Insert at position \n");
+                printf("Enter your Choice : ");
+                int flag1 ;
+                scanf("%d",&flag1);
+                if(flag1 == 1)
+                    head = insert_first(head);
+                else if(flag1 == 2)
+                    insert_last(head);
+                else if(flag1 == 3)
+                    insert_pos(head);
+                else
+                    printf("Wrong Choice !!\n");
+                break;
+            case 5 :
+                printf("1. Delete at first \n");
+                printf("2. Delete at Last \n");
+                printf("3. Delete at position \n");
+                printf("Enter your Choice : ");
+                int flag2 ;
+                scanf("%d",&flag2);
+                if(flag2 == 1)
+                    head = delete_first(head);
+                else if(flag2 == 2)
+                    delete_last(head);
+                else if(flag2 == 3)
+                    delete_pos(head);
+                else
+                    printf("Wrong Choice !!\n");
+                break;
+            case 6 :
                 choice = 0;
                 break;
             default :
@@ -78,4 +119,67 @@ void displayList(struct Node* head){
         printf("%d \n",ptr->data);
         ptr = ptr->next;
     }
+}
+struct Node* insert_first(struct Node* head){
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter the Data : ");
+    scanf("%d",&new->data);
+    new->next = head;
+    return new;
+}
+void insert_last(struct Node* head){
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter the Data : ");
+    scanf("%d",&new->data);
+    new->next = NULL;
+    struct Node* ptr = head;
+    while(ptr->next != NULL){
+        ptr = ptr->next;
+    }
+    ptr->next = new;
+}
+void insert_pos(struct Node* head){
+    int pos;
+    printf("Enter the Position : ");
+    scanf("%d",&pos);
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter the Data : ");
+    scanf("%d",&new->data);
+    struct Node* ptr = head ;
+    --pos;
+    while(ptr != NULL && --pos){
+        ptr = ptr->next;
+    }
+    new->next = ptr->next;
+    ptr->next = new;
+}
+struct Node* delete_first(struct Node* head){
+    struct Node* temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+void delete_last(struct Node* head){
+    struct Node* temp,*ptr;
+    ptr = head;
+    while((ptr->next)->next != NULL){
+        ptr = ptr->next;
+    }
+    temp = ptr->next;
+    ptr->next = NULL;
+    free(temp);
+}
+void delete_pos(struct Node* head){
+    int pos;
+    printf("Enter the Position : ");
+    scanf("%d",&pos);
+    struct Node* temp , *ptr;
+    ptr = head;
+    --pos;
+    while(ptr != NULL && --pos){
+        ptr = ptr->next;
+    }
+    temp = ptr->next;
+    ptr->next = (ptr->next)->next;
+    free(temp);
 }
