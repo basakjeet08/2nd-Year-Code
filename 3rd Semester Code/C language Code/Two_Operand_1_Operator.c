b774c1bf2af598da0caee9722411c0ca058c5774
Anirban Basak <jeetbasak2002@gmail.com>
2022-09-06 22:36:20 +0530

    06/09/2022

---
+++ 3rd Semester Code/C language Code/Two_Operand_1_Operator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    char operand;
+    int num1 , num2 ;
+    float result;
+
+    printf("Enter two Operands (a,b) : ");
+    scanf("%d %d",&num1 , &num2);
+    getchar();
+    printf("Enter the Operation (+,*,-,/,%%) : ");
+    scanf("%c",&operand);
+
+    switch(operand){
+        case '*' :
+            result= num1*num2;
+            break;
+        case '/' : 
+            result= (float)num1 / num2;
+            break;
+        case '+' : 
+            result = num1+num2;
+            break;
+        case '-' : 
+            result = num1-num2;
+            break;
+        case '%' : 
+            result = num1%num2;
+            break;
+        default : 
+            printf("Wrong Input !!");
+    }
+    printf("The Result : %f",result);
+    return 0;
+}
