6fcf68320cd150be0b20b9ef28b7e0976d5e4568
Anirban Basak <jeetbasak2002@gmail.com>
2022-09-07 01:53:31 +0530

    07/09/2022

---
+++ 3rd Semester Code/C language Code/Binary_Decimal_Conversion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int num;
+    long long int binary;
+    binary = 0;
+    printf("Enter the Number : ");
+    scanf("%d",&num);
+
+    while(num>0){
+        int rem = num%2;
+        binary = binary*10 + rem;
+        num/= 2;
+    }
+    printf("The Binary : %ld\n",binary);
+    int count = 0;
+    printf("%d",num);
+    while(binary>0){
+        int rem = binary%10;
+        num = num + rem*(pow(2,count));
+        count++;
+        binary/= 10;
+    }
+    printf("The Number was : %d",num);
+}
