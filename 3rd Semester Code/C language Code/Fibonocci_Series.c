5431ce2d87f4b5cbfea5ccab66ece96ae51af6a9
Anirban Basak <jeetbasak2002@gmail.com>
2022-09-07 01:16:50 +0530

    07/09/2022

---
+++ 3rd Semester Code/C language Code/Fibonocci_Series.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){    
+    int num1=0,num2=1,num3,range;    
+    printf("Enter the number of elements:");    
+    scanf("%d",&range);    
+    printf("\n%d %d",num1,num2);
+    for(int i=2;i<range;++i){    
+        num3=num1+num2;    
+        printf(" %d",num3);    
+        num1=num2;    
+        num2=num3;    
+    }  
+    return 0;  
+}   
