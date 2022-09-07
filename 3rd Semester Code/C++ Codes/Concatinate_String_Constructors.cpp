ca93c763c135154cf3bbde49ee007fbcbd3b2371
Anirban Basak <jeetbasak2002@gmail.com>
2022-09-07 09:44:52 +0530

    07/09/2022

--- 3rd Semester Code/C++ Codes/Concatinate_String_Constructors.cpp
+++ 3rd Semester Code/C++ Codes/Concatinate_String_Constructors.cpp
@@ -3,8 +3,38 @@ using namespace std;
 
 class Strings{
     string st;
+    int len;
     public : 
         Strings(){
-            
+            st = "";
+            len = 0;
         }
+        Strings(string st){
+            this->st = st;
+            len = st.length();
+        }
+        Strings(Strings* ob){
+            st = ob->st;
+            len = ob->len;
+        }
+        void showData(){
+            cout << "The String is : " << st << " of length " << len << endl;
+        }
+        void concatStrings(string s){
+            st = st + s;
+            len = len + s.length();
+        }
+};
+int main(){
+    Strings ob1;
+    Strings ob2("Anirban");
+    Strings ob3(ob2);
+    ob1.concatStrings("Hello");
+    ob2.concatStrings(" Basak");
+    ob3.concatStrings("\n");
+
+    ob1.showData();
+    ob2.showData();
+    ob3.showData();
+    return 0;
 }
