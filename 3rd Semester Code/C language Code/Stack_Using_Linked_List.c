0b438d99d94c5fa0a63a096e8b06ed264f29c38b
Anirban Basak <jeetbasak2002@gmail.com>
2022-09-16 01:18:15 +0530

    16/09/2022

---
+++ 3rd Semester Code/C language Code/Stack_Using_Linked_List.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node* next ;
+};
+struct Node* Push(struct Node* );
+struct Node* Pop(struct Node*);
+void showStack(struct Node* );
+int main(){
+    int choice;
+    struct Node* top = NULL;
+    do{
+        printf("--------------------Menu --------------------\n");
+        printf("1. PUSH into Stack \n");
+        printf("2. POP into Stack \n");
+        printf("3. Display Stack \n");
+        printf("4. Exit \n");
+        printf("\nEnter your Choice : ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1 : 
+                top = Push(top);
+                break ;
+            case 2 : 
+                top = Pop(top);
+                break ;
+            case 3 : 
+                showStack(top);
+                break;
+            case 4 : 
+                break ;
+            default : 
+                printf("Wrong Input !! Try Again !! \n"); 
+        }
+    }while(choice != 4);
+    return 0 ;
+}
+struct Node* Push(struct Node* top){
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter the Data : ");
+    scanf("%d",&new->data);
+    new->next = top ;
+    return new;
+}
+struct Node*  Pop(struct Node* top){
+    if(top == NULL){
+        printf("Stack Underflow !! \n");
+        return top ;
+    }
+    struct Node* temp = top ;
+    top = top->next;
+    free(temp);
+    return top;
+}
+void showStack(struct Node* top){
+    while(top != NULL){
+        printf("%d ",top->data);
+        top = top->next;
+    }
+    printf("\n");
+}
