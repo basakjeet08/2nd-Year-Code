ae855e423c09bfee6283d623088eac3d95ecf8d9
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-12 10:06:08 +0530

    12/10/2022

---
+++ 3rd Semester Code/C++ Codes/Lab_5_Q6.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std ;
+class employee{
+    protected :
+        string name;
+        int id;
+        double salary;
+        void getDataEmployee(){
+            cout << "Enter the Name : ";
+            getline(cin , name);
+            cout << "Enter the Id : " ;
+            cin >> id ;
+        }
+        void showData(){
+            cout << "Name : " << name << endl ;
+            cout << "ID : " << id << endl ;
+            cout << "Salary : " << salary << endl ;
+        }
+};
+class PartTime : private employee{
+    int hours,pay;
+    public : 
+        void getData(){
+            getDataEmployee();
+            cout << "Enter the Amount of Hours : ";
+            cin >> hours;
+            cout << "Enter the Salary per hour : " ;
+            cin >> pay ;
+            getchar();
+            calSalary();
+        }
+        void calSalary(){
+            salary = hours*pay ;
+            showData();
+        }
+};
+class RegularTime : private employee{
+    double basic_salary,da, hra ;
+    public :
+        void getData(){
+            getDataEmployee();
+            cout << "Enter the Basic Salary : ";
+            cin >> basic_salary ;
+            cout << "Enter the DA : ";
+            cin >> da ;
+            cout << "Enter the HRA : ";
+            cin >> hra ;
+            getchar();
+            calSalary();
+        }
+        void calSalary(){
+            salary = da + basic_salary + hra ;
+            showData();
+        }
+};
+int main(){
+    RegularTime r1;
+    PartTime p1 ;
+    r1.getData();
+    p1.getData();
+    return 0 ;
+}
