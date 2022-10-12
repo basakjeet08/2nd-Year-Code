a5b9a7cbb620854fdf58939a54eebf05c0dff0cb
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-12 10:06:58 +0530

    Update Lab_5_Q6.cpp

--- 3rd Semester Code/C++ Codes/Lab_5_Q6.cpp
+++ 3rd Semester Code/C++ Codes/Lab_5_Q6.cpp
@@ -1,3 +1,10 @@
+/*
+Q6. Create a class which stores employee name,id and salary Derive two classes from
+‘Employee’ class: ‘Regular’ and ‘Part-Time’. The ‘Regular’ class stores DA, HRA and
+basic salary. The ‘Part-Time’ class stores the number of hours and pay per hour.
+Calculate the salary of a regular employee and a par-time employee.
+*/
+
 #include <iostream>
 using namespace std ;
 class employee{
