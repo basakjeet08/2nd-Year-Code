1fa7ec576fde49e362feabe6b69b28beac3bf6e3
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-13 23:42:33 +0530

    13/10/2022

---
+++ 3rd Semester Code/C language Code/Sparce_Matrix_Addition_Using_Matrix.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+void formTriplet(int*,int*, int);
+int nonZeroElements(int* , int);
+void addSparceMatrix(int* , int* , int , int);
+void showData(int* , int);
+int main(){
+    int size;
+    printf("Enter the Size of the Array : ");
+    scanf("%d",&size);
+    int matrix1[size][size];
+    int matrix2[size][size];
+    printf("Enter the Value of first Matrix : \n");
+    for(int i=0;i<size;i++)
+        for(int j=0;j<size;j++){
+            printf("Element [%d][%d] : " , i+1 , j+1);
+            scanf("%d",&matrix1[i][j]);
+        }
+    printf("Enter the Value of Second Matrix : \n");
+    for(int i=0;i<size;i++)
+        for(int j=0;j<size;j++){
+            printf("Element [%d][%d] : " , i+1 , j+1);
+            scanf("%d",&matrix2[i][j]);
+        }
+    formTriplet(matrix1[0] ,matrix2[0] , size);
+}
+void formTriplet(int* matrix1 , int* matrix2, int size){
+    int non_0_ele_1 = nonZeroElements(matrix1 , size);
+    int non_0_ele_2 = nonZeroElements(matrix2 , size);
+    int triplet1[non_0_ele_1][3];
+    int triplet2[non_0_ele_2][3];
+    int flag = 0; 
+    for(int i=0;i<size;i++)
+        for(int j = 0;j<size ;j++){
+            if(*matrix1 != 0){
+                triplet1[flag][0] = i ;
+                triplet1[flag][1] = j ;
+                triplet1[flag][2] = *matrix1 ;
+                flag++;
+            }
+            matrix1++;
+        }
+    flag = 0;
+    for(int i=0;i<size;i++)
+        for(int j = 0;j<size ;j++){
+            if(*matrix2 != 0){
+                triplet2[flag][0] = i ;
+                triplet2[flag][1] = j ;
+                triplet2[flag][2] = *matrix2 ;
+                flag++;
+            }
+            matrix2++;
+        }
+    addSparceMatrix(triplet1[0] , triplet2[0] , non_0_ele_1 , non_0_ele_2);
+}
+int nonZeroElements(int *matrix , int nos){
+    int count = 0;
+    for(int i = 0;i<nos*nos;i++){
+        if((*matrix) != 0)
+            count++;
+        matrix++;
+    }
+    return count;
+}
+void adder(){
+    
+}
+void addSparceMatrix(int* triplet1 , int* triplet2 , int t1_size , int t2_size){
+    int triplet3[t1_size+t2_size][3];
+    int* ptr = triplet3[0];
+    int flag = -1 ;
+    while(t1_size != 0 && t2_size != 0){
+        flag++;
+        if(*triplet1 < *triplet2){
+            triplet3[flag][0] = *triplet1 ;
+            triplet3[flag][1] = *(triplet1+1);
+            triplet3[flag][2] = *(triplet1+2);
+            t1_size--;
+            triplet1+=3;
+        }
+        else if(*triplet1 > *triplet2){
+            triplet3[flag][0] = *triplet2 ;
+            triplet3[flag][1] = *(triplet2+1);
+            triplet3[flag][2] = *(triplet2+2);
+            t2_size--;
+            triplet2+= 3;
+        }
+        else{
+            if(*(triplet1+1) > *(triplet2+1)){
+                triplet3[flag][0] = *triplet2 ;
+                triplet3[flag][1] = *(triplet2+1);
+                triplet3[flag][2] = *(triplet2+2);
+                t2_size--;
+                triplet2+= 3;
+            }
+            else if(*(triplet1+1) < *(triplet2+1)){
+                triplet3[flag][0] = *triplet1 ;
+                triplet3[flag][1] = *(triplet1+1);
+                triplet3[flag][2] = *(triplet1+2);
+                t1_size--;
+                triplet1+=3;
+            }
+            else{
+                triplet3[flag][0] = *triplet1 ;
+                triplet3[flag][1] = *(triplet1+1);
+                triplet3[flag][2] = *(triplet1+2) + *(triplet2+2);
+                t1_size--;
+                t2_size--;
+                triplet1+=3;
+                triplet2+=3;
+            }
+        }
+    }
+    int* leftover = triplet2;
+    int size = t2_size;
+    if(t1_size != 0){
+        leftover = triplet1;
+        size = t1_size ;
+    }
+    for(int i=0;i<size;i++){
+        flag++;
+        triplet3[flag][0] = *leftover ;
+        triplet3[flag][1] = *(leftover+1);
+        triplet3[flag][2] = *(leftover+2);
+        leftover+=3;
+    }
+    showData(triplet3[0] , flag);
+}
+
+void showData(int* matrix , int size){
+    for(int i=0;i<=size;i++){
+        for(int j=0;j<3;j++){
+            printf("%d " , *matrix);
+            matrix++;
+        }
+        printf("\n");
+    }
+}
