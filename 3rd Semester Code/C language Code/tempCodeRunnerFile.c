8d87427857769988770604e03c983e54270de124
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-21 13:14:06 +0530

    21/10/2022

--- 3rd Semester Code/C language Code/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-= ptr1 ;
-                temp->data = ptr1->data + ptr2->data;
-                adder(&head3 , &ptr3 , &temp);
-                ptr1 = ptr1->next;
-                ptr2 = ptr2->next;
