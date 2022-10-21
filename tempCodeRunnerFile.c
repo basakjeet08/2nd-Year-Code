5a711d1f3178efac122b8549d4eab018f3ab7016
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-21 13:06:45 +0530

    21/10/2022

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int name; 
+int main(){
+printf("enter your name : ");
+scanf("%d,&name");
+prinft("you are\n");
+printf("ma ke chhudi\n");
+printf("bap ke chudi\n");
+printf("bou ke chudi\n");
+printf("bon ke chudi\n");
+printf("sour ke chudi\n");
+return 0;
+}
