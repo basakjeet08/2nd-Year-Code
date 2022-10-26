44408bc01dd7f4abecadd798d25214208ad8a427
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-26 10:34:16 +0530

    26/10/2022

--- 3rd Semester Code/C++ Codes/Type_Casting_Class_by_Constructor.cpp
+++ 3rd Semester Code/C++ Codes/Type_Casting_Class_by_Constructor.cpp
@@ -1,19 +1,18 @@
+/*
+    3-WAP to convert Class type to Class type.
+        i- By using constructor
+*/
 #include <iostream>
-using namespace std ;
+using namespace std;
 class Number1{
     int a , b ;
     public : 
-        Number1(){
-            a = b = 0;
-        }
         Number1(int x , int y){
             a = x ;
             b = y ;
             cout << "Parameterized Constructor Called !!" << endl;
         }
-        void showData(){
-            cout << "A : "<< a << " B : " << b << endl ;
-        }
+        void showData(){cout << "A : " << a << " B : " << b << endl ;}
 };
 class Number2{
     int x , y ;
@@ -22,22 +21,17 @@ class Number2{
             cout << "Enter Data : " ;
             cin >> x >> y ;
         }
-        int getter_x(){
-            return x ;
-        }
-        int getter_y(){
-            return y ;
-        }
+        int getter_x(){return x ;}
+        int getter_y(){return y ;}
 };
 int main(){
     Number2 num2;
     num2.getData();
 
-    int t1 = num2.getter_x();
-    int t2 = num2.getter_y();
+    int temp1 = num2.getter_x();
+    int temp2 = num2.getter_y();
 
-    Number1 num1 ;
-    num1 = Number1(t1 , t2);
+    Number1 num1 = Number1(temp1 , temp2);
     num1.showData();
     return 0 ;
 }
