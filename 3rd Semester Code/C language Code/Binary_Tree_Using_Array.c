58a93945cfd2f802259947daee5474b00fce5f17
Anirban Basak <jeetbasak2002@gmail.com>
2022-11-03 01:47:02 +0530

    03/11/2022

--- 3rd Semester Code/C language Code/Binary_Tree_Using_Array.c
+++ 3rd Semester Code/C language Code/Binary_Tree_Using_Array.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 void buildTree(int[] , int , int);
+void showInOrder(int* , int);
 int main(){
     int tree[20] , data ;
-    printf("Anirban Basak \n21051880\n");
     for(int i=0;i<20;i++)
         tree[i] = -1 ;
     printf("Enter the Root Node : ");
     scanf("%d",&data);
     buildTree(tree , 0 , data);
-
-    for(int i = 0;i<20;i++){
-        if(tree[i] == -1)
-            printf("__");
-        else
-            printf("%d " ,tree[i]);
-    }
+    showInOrder(tree , 0);
     return 0;
 }
 void buildTree(int tree[] , int index , int item){
@@ -34,4 +28,12 @@ void buildTree(int tree[] , int index , int item){
         scanf("%d" , &data);
         buildTree(tree , ((index*2)+2) , data);
     }
+}
+void showInOrder(int* tree , int index){
+    if(((index*2)+1) < 20)
+        showInOrder(tree , ((2*index)+1));
+    if(*(tree+index) != -1)
+        printf("%d " , *(tree + index));
+    if(((index*2)+2) < 20)
+        showInOrder(tree , ((2*index)+2));
 }
