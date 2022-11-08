33917562b3b330cbe98204fdf1ef20f0cb0ac650
Anirban Basak <jeetbasak2002@gmail.com>
2022-11-08 18:44:17 +0530

    08/11/2022

--- 3rd Semester Code/C++ Codes/Lab_6_Q2.cpp
+++ 3rd Semester Code/C++ Codes/Lab_6_Q2.cpp
@@ -2,19 +2,30 @@
 2. Create a class to store an integer array. Overload insertion and extraction operator to
 input and display the array elements.
 */
-
 #include <iostream>
-using namespace std ;
+using namespace std;
 class A{
-    int arr[5];
-    public : 
-        istream &operator >>(istream &input , A ob){
-            for(int i = 0;i<5;i++)
-                input >> ob.arr[i];
-            return *ob ;
-        }
-};
+    public:
+        int arr[5];
+        friend ostream &operator<<(ostream &out, A &obj);
+        friend istream &operator>>(istream &in, A &obj);
+}; 
+istream &operator>>(istream &in, A &obj){
+    for (int i=0; i<5; i++)
+        in >> obj.arr[i];
+    return in;
+}
+ostream &operator<<(ostream &out, A &obj){
+    for (int i=0; i<5; i++)
+        out << obj.arr[i] << " ";
+    cout << endl;
+    return out;
+}
 int main(){
-    A ob1 , ob2 ;
-    cin >> ob1 ;
+    A obj;
+    cout << "Enter 5 integers into array: ";
+    cin >> obj;
+    cout << "The array elements are: ";
+    cout << obj;
+    return 0;
 }
