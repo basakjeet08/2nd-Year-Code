479c1c5411c281c1dc08b82ef1269796335670b5
Anirban Basak <jeetbasak2002@gmail.com>
2022-11-11 10:50:28 +0530

    11/11/2022

---
+++ 3rd Semester Code/C language Code/Binary_Search_Tree_uisng_LL.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Tree {
+    int data ;
+    struct Tree *lc , *rc ;
+};
+struct Tree* buildTree(struct Tree*);
+void showTree(struct Tree*);
+int main(){
+    int choice ;
+    struct Tree* root = (struct Tree*)malloc(sizeof(struct Tree)) ;
+    printf("Enter the Root Node : ");
+    scanf("%d",&root->data);
+    root->lc = root->rc = NULL ;
+    do{
+        buildTree(root);
+        printf("Do you want to enter another Element (1/0) : ");
+        scanf("%d",&choice);
+    }while(choice);
+    showTree(root);
+}
+struct Tree* buildTree(struct Tree* root){
+    int choice ;
+    struct Tree* ptr = root ;
+    struct Tree* prev = NULL ;
+    struct Tree* new = (struct Tree*)malloc(sizeof(struct Tree));
+    printf("Enter the Data : ");
+    scanf("%d",&new->data);
+    new->lc = new->rc = NULL ;
+    while(1){
+        if(ptr->data < new->data){
+            if(ptr->rc !=NULL)
+                ptr = ptr->rc ;
+            else{
+                ptr->rc = new ;
+                break;
+            }
+        }
+        else{
+            if(ptr->lc != NULL)
+                ptr = ptr->lc ;
+            else{
+                ptr->lc = new ;
+                break;
+            }
+        }
+    }
+    return root ;
+}
+void showTree(struct Tree* root){
+    if(root->lc != NULL)
+        showTree(root->lc);
+    printf("%d " , root->data);
+    if(root->rc != NULL)
+        showTree(root->rc);
+}
