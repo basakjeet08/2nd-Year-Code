cef15d3ae945ca6f6d96ecbf2cf3e612dd853cfb
Anirban Basak <jeetbasak2002@gmail.com>
2022-11-14 09:08:18 +0530

    14/11/2022

---
+++ 3rd Semester Code/C++ Codes/File_Input_Output.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+    string input ;
+    string output ;
+    ofstream myFileWriter("File.txt");
+    
+    cout << "Enter the data to be added to the File : " << endl;
+    for(int i = 0 ;i<3 ;i++){
+        getline(cin , input);
+        myFileWriter << input;
+    }
+    ifstream myFileReader("File.txt");
+    cout << "--------------------OUTPUT--------------------\n";
+    while(!myFileReader.eof()){
+        getline(myFileReader,output) ;
+        cout << output << endl ;
+    }
+    return 0 ;
+}
