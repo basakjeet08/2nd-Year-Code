6bd239ed97dde03f46f157e7855f6dbb4a0480a9
Anirban Basak <jeetbasak2002@gmail.com>
2022-12-19 02:17:13 +0530

    joint commit

---
+++ 3rd Semester Code/Practise/OOP/Test05.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// class SquareMatrix{
+//     int* ptr , size ;
+//     public :
+//         SquareMatrix(int size){
+//             this->size = size ;
+//             ptr = new int[size][size];
+//         }
+//         void getData(){
+//             cout << "Enter the Array Elements : " ;
+//             for(int i = 0;i<size;i++)
+//                 for(int j = 0;j<size;j++)
+//                     cin >> ptr[i][j];
+//         }
+        // SquareMatrix operator+(){
+        //     SquareMatrix temp(size);
+        //     for(int i = 0;i<size;i++)
+        //         for(int j = 0 ;j<size ; j++){
+        //             temp.ptr[i][j];
+        //         }
+        // }        
+// };
+int main(){
+    // SquareMatrix s1(2);
+    // s1.getData();
+
+    
+}
