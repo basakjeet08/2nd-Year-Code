8e5e7dee283a5b866d049898e9ff262e76f155a8
Anirban Basak <jeetbasak2002@gmail.com>
2023-02-19 23:21:51 +0530

    Improved the Code

--- 4th Semester Code/Web Technology/Lab 05/Question_01.java
+++ 4th Semester Code/Web Technology/Lab 05/Question_01.java
@@ -12,15 +12,13 @@ public class Question_01 {
         System.out.println("Enter the Two Numbers(Int) : ");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int res1 = obj.subtract(a, b);
-        System.out.println("The Result is : " + res1);
+        System.out.println("The Result is : " + obj.subtract(a, b));
 
 
         System.out.print("Enter Two Numebers(Double) : ");
         double num1 = sc.nextDouble();
         double num2 = sc.nextDouble();
-        double res2 = obj.subtract(num1, num2);
-        System.out.print("The Result is : " + res2);
+        System.out.print("The Result is : " + obj.subtract(num1, num2));
     
         
         sc.close();
--- 4th Semester Code/Web Technology/Lab 05/Question_02.java
+++ 4th Semester Code/Web Technology/Lab 05/Question_02.java
@@ -5,45 +5,45 @@ class Student {
     String name;
     float cgpa;
 
-    Student(int roll, String name, float cgpa) {
-        this.roll = roll;
-        this.name = name;
-        this.cgpa = cgpa;
-    }
-
-    void display() {
+    void showData() {
         System.out.println("Roll: " + roll);
         System.out.println("Name: " + name);
         System.out.println("CGPA: " + cgpa);
+    }
+
+    void getData(Scanner sc){
+        System.out.print("Roll: ");
+        roll = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Name: ");
+        name = sc.nextLine();
+        System.out.print("CGPA: ");
+        cgpa = sc.nextFloat();
     }
 }
 
 public class Question_02
 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of students: ");
         int n = sc.nextInt();
         Student[] students = new Student[n];
+
         for (int i = 0; i < n; i++) {
             System.out.println("Enter details of student " + (i + 1));
-            System.out.print("Roll: ");
-            int roll = sc.nextInt();
-            System.out.print("Name: ");
-            String name = sc.next();
-            System.out.print("CGPA: ");
-            float cgpa = sc.nextFloat();
-            students[i] = new Student(roll, name, cgpa);
+            students[i] = new Student();
+            students[i].getData(sc);
         }
-        float min = students[0].cgpa;
-        int minIndex = 0;
-        for (int i = 1; i < n; i++) {
-            if (students[i].cgpa < min) {
-                min = students[i].cgpa;
-                minIndex = i;
-            }
-        }
+
+        sc.close();
+        Student min = students[0];
+        for (int i = 1; i < n; i++)
+            if (students[i].cgpa < min.cgpa)
+                min = students[i];
+
         System.out.println("Student with lowest CGPA:");
-        students[minIndex].display();
+        min.showData();
     }
 }
