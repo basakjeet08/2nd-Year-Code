e9e0d6693f1df875aa7a720aed660b180eb2d2ae
Anirban Basak <jeetbasak2002@gmail.com>
2023-05-08 05:09:51 +0530

    08/05/2023

--- 4th Semester Code/Web Technology/Practise/PhonePractise/BootLoader.java
+++
@@ -1,7 +0,0 @@
-package PhonePractise;
-
-public class BootLoader {
-    public static void main(String args[]){
-        DataTypeSizeDetector dataTypeSizeDetector = new DataTypeSizeDetector();
-    }
-}
